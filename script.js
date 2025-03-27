30135eb7cdcaba077e3e3076ca3cb94ed698f854
GuanSkuan <jovanstojkovv@gmail.com>
2025-03-27 22:57:43 +0100

    dodate sve slike za "o nama" stranicu, dodat css za navigaciju i footer, a i za slike i tekst, dodata mini ikonica za tab, footer sa navigacionim linkovima i footer html koji se povezao sa js fajlom

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+/*NAVIGACIJA*/
+
 fetch("navigacija.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("navigacija").innerHTML = data;
     })
     .catch(error => console.error("Greska: ucitavanje navigacionog bara", error));
+
+/*FOOTER*/
+
+fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+    })
+    .catch(error => console.error("Greska: ucitavanje footera", error));
