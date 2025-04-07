b537a156f3144dc4c2028c239909dd8d43164b34
Skuan <jovanstojkovv@gmail.com>
2025-04-07 19:51:17 +0200

    forma je kompletno gotova i ceo sajt je zavrsen

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ fetch("footer.html")
         document.getElementById("footer").innerHTML = data;
     })
     .catch(error => console.error("Greska: ucitavanje footera", error));
+
+/*FORMA*/
+
+document.querySelector(".forma").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const ime = document.getElementById("ime").value;
+    const prezime = document.getElementById("prezime").value;
+    const pol = document.querySelector('input[name="pol"]:checked')?.value;
+    const email = document.getElementById("email").value;
+    const tema = document.getElementById("tema").value;
+    const poruka = document.getElementById("poruka").value;
+    const uslovi = "Da";
+
+    alert(
+        "Poslati podaci:\n\n" +
+        "Ime: " + ime + "\n" +
+        "Prezime: " + prezime + "\n" +
+        "Pol: " + pol + "\n" +
+        "Email: " + email + "\n" +
+        "Tema poruke: " + tema + "\n" +
+        "Poruka: " + poruka + "\n" +
+        "Slažem se sa uslovima: " + uslovi
+    );
+
+    // Resetovanje forme
+    this.reset();
+
+    setTimeout(() => {
+        location.reload();
+    }, 100);
+});
